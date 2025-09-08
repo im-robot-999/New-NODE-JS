6d1157a21ced49b1d5f84cd6905f309686c6988c
I'm ROBOT <robotim21@gmail.com>
2025-09-08 15:01:11 +0530

    Update 03_calculator_module.js

--- 03_calculator_module.js
+++ 03_calculator_module.js
@@ -1,7 +1,32 @@
-exports.add = (a, b) => a + b;
-exports.sub = (a, b) => a - b;
-exports.mul = (a, b) => a * b;
-exports.div = (a, b) => a / b;
-// Usage demo
-// const calc = require('./03_calculator_module');
-// console.log(calc.add(2, 3));
+const readline = require('readline');
+
+const calc = {
+  add: (a, b) => a + b,
+  sub: (a, b) => a - b,
+  mul: (a, b) => a * b,
+  div: (a, b) => a / b
+};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter first number: ', (a) => {
+  rl.question('Enter second number: ', (b) => {
+    rl.question('Choose operation (+, -, *, /): ', (op) => {
+      a = parseFloat(a);
+      b = parseFloat(b);
+      let result;
+      switch(op) {
+        case '1': result = calc.add(a, b); break;
+        case '2': result = calc.sub(a, b); break;
+        case '3': result = calc.mul(a, b); break;
+        case '4': result = calc.div(a, b); break;
+        default: result = 'Invalid operation';
+      }
+      console.log(`// Result: ${result}`);
+      rl.close();
+    });
+  });
+});
