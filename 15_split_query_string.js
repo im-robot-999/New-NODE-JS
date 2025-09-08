69b8d80b792113ad471c765dc63051a7f7ed0c5e
I'm ROBOT <robotim21@gmail.com>
2025-09-08 15:40:22 +0530

    Update 15_split_query_string.js

--- 15_split_query_string.js
+++ 15_split_query_string.js
@@ -3,5 +3,8 @@ const url = require('url');
 http.createServer((req, res) => {
   const q = url.parse(req.url, true).query;
   for (const k in q) console.log(`${k}: ${q[k]}`);
+  console.log('All query parameters have been logged.');
   res.end('Split logged');
-}).listen(3000);
+}).listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
