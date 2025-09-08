e6e7d11ba0b015f7ba587e1e98d473388a71c322
I'm ROBOT <robotim21@gmail.com>
2025-09-08 15:34:41 +0530

    Update 11_serve_requested_file.js

--- 11_serve_requested_file.js
+++ 11_serve_requested_file.js
@@ -4,6 +4,14 @@ const url = require('url');
 http.createServer((req, res) => {
   const p = url.parse(req.url).pathname.substr(1);
   fs.readFile(p, (e, d) => {
-    res.end(e ? '404' : d);
+    if (e) {
+      console.log(`File not found: ${p}`);
+      res.end('404');
+    } else {
+      console.log(`File served: ${p}`);
+      res.end(d);
+    }
   });
-}).listen(8080);
+}).listen(8080, () => {
+  console.log('Server running at http://localhost:8080/');
+});
