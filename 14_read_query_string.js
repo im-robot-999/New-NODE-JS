7bf49793a45beb8602b62881b72f9ddbc5ae91b8
I'm ROBOT <robotim21@gmail.com>
2025-09-08 15:33:47 +0530

    Update 14_read_query_string.js

--- 14_read_query_string.js
+++ 14_read_query_string.js
@@ -2,6 +2,8 @@ const http = require('http');
 const url = require('url');
 http.createServer((req, res) => {
   const q = url.parse(req.url, true).query;
-  console.log(q);
+  console.log('Received query:', q); // Log the query object
   res.end('Query logged');
-}).listen(3000);
+}).listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
